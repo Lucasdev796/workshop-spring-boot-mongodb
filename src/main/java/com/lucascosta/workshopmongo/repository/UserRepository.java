91d49be89f8c109ded4e976cbbf136e2452ee456
Lucas Costa <lucas.desenvolvedor.tec@gmail.com>
2024-12-12 12:45:37 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lucascosta/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lucascosta.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucascosta.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
