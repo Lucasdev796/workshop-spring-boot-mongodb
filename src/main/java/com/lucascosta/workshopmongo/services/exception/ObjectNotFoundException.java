50528afdeb3439531a38ab152cb23fbf4817f4aa
Lucas Costa <lucas.desenvolvedor.tec@gmail.com>
2024-12-13 16:27:10 -0300

    Obtendo um post por id

--- src/main/java/com/lucascosta/workshopmongo/services/exception/ObjectNotFoundException.java
+++ src/main/java/com/lucascosta/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -1,5 +1,9 @@
 package com.lucascosta.workshopmongo.services.exception;
 
-public class ObjectNotFoundException {
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
 }
